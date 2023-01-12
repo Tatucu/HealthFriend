642ae4f490f752a94bcd121b3a1dadf0150f5d3a
Tatucu <andreid2312@gmail.com>
2023-01-12 14:14:34 +0100

    Initial Commit

---
+++ HealthFriend Watch App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  HealthFriend Watch App
+//
+//  Created by user230454 on 1/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HealthFriend Watch App/HealthFriendApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthFriendApp.swift
+//  HealthFriend Watch App
+//
+//  Created by user230454 on 1/12/23.
+//
+
+import SwiftUI
+
+@main
+struct HealthFriend_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
